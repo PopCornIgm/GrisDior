3b58ac690c37dbe49dc642625b01d8e8a6530803
PopCornIgm <popcornigorek.1993@gmail.com>
2024-05-22 19:49:02 +0700

    landing
    slider addition

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 'use strick';
-const swiper = new Swiper('.swiper', {
-  loop: true,
+const swiper = new Swiper('.top__swiper', {
+  effect: 'fade',
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperAbout = new Swiper('.about__slider', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
